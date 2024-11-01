3a3038e17b9d45b5a495a7bda964fdd578d5e654
Sergey Kovalchuk <81366846+SergeyKovalchuk2086@users.noreply.github.com>
2024-11-01 19:35:21 +0700

    add pwa icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -269,6 +269,10 @@ export default {
   build: {},
 
   pwa: {
+    icon: {
+      source: "/react-logo.png",
+      sizes: [64, 120, 144, 152, 192, 384, 512],
+    },
     meta: {
       title: "TestTitle",
       author: "Ya",
